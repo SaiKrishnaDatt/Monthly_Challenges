1d80a8fd1f469d2ade034e7a2fcceb932153e986
krishnadutt <krishna.suddhapally@gmail.com>
2022-09-11 01:53:56 +0530

    dynamic path support

--- challenges/views.py
+++ challenges/views.py
@@ -1,7 +1,21 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse,HttpResponseNotFound
 
 # Create your views here.
 
-def index(request):
+def january(request):
     return HttpResponse("This works!")
+def february(request):
+    return HttpResponse("Below are the todo for february")
+
+def monthly_challenges(request,month):
+    responseText = None;
+    if month == "january":
+        responseText= "5K per day"
+    elif month == "february" :
+        responseText = "5K per day + 30mins yoga"
+    elif month == "march" :
+        responseText = "10K per day"
+    else :
+        return HttpResponseNotFound("The url is incorrect, month at the end of the url is not valid one");
+    return HttpResponse(responseText)
+
